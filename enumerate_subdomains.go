a810165afb79781a402c9fb6ca45dfc2e59e3b1e
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-06-28 13:45:38 -0400

    add ascii flowchart with documentation for EnumerateSubdomains function

--- enumerate_subdomains.go
+++ enumerate_subdomains.go
@@ -2,6 +2,19 @@ package subzero
 
 import "sync"
 
+// EnumerateSubdomains takes the given domain and with each Source from EnumerationOptions,
+// it will spawn a go routine to start processing that Domain. The result channels from each
+// source are merged into one results channel to be consumed.
+//
+//
+//
+//   ____________________________     Source1.ProcessDomain     ___________        _____
+//  |                            | /                         \ |           |      |     |
+//  | EnumerationOptions.Sources | -- Source2.ProcessDomain -- |  Results  | ---> |  ?  |
+//  |____________________________| \                         / |___________|      |_____|
+//                                    Source3.ProcessDomain
+//
+//
 func EnumerateSubdomains(domain string, options *EnumerationOptions) <-chan *Result {
 	results := make(chan *Result)
 	go func(domain string, options *EnumerationOptions, results chan *Result) {
